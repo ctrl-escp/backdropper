ddb06f9e9e7616fcec30b8dffde19637ca0d045e
Ben Baryo <19845603+ctrl-escp@users.noreply.github.com>
2023-08-02 10:28:02 +0300

    Change authentication method to allow CLI process to receive an access token and a refresh token

--- backdropper.py
+++ backdropper.py
@@ -3,22 +3,52 @@
 Backdropper - Backup to Dropbox.
 """
 import os
+import json
 import dropbox
 import argparse
 from dropbox.files import WriteMode
 from zipfile import ZipFile, ZIP_BZIP2
 
-ACCESS_TOKEN_FILENAME: str = ".token"
+SECRETS_FILENAME = ".secrets.json"
 
 
 class Backdropper:
 
-	def __init__(self, token: str):
-		self.token: str = token
-		self.dbx: dropbox.Dropbox = dropbox.Dropbox(self.token)
-		self.path: str = ''
+	def __init__(self, secrets_filename = SECRETS_FILENAME):
+		self.secrets_filename = secrets_filename
+		self.secrets: dict = {}
+		self.dbx: dropbox.Dropbox | None = None
+		self.path: str = ""
 		self.overwrite: bool = False
 		self.success: bool = False
+		self._get_secrets()
+		self._authorize()
+
+	def _get_secrets(self):
+		with open(self.secrets_filename) as f:
+			self.secrets: dict = json.loads(f.read())
+
+	def _save_secrets(self):
+		with open(self.secrets_filename, "w", encoding="utf-8") as f:
+			f.write(json.dumps(self.secrets, indent=2))
+
+	def _authorize(self):
+		if self.secrets.get("refresh_token"):
+			self.dbx = dropbox.Dropbox(app_key=self.secrets["app_key"], app_secret=self.secrets["app_secret"],
+									   oauth2_refresh_token=self.secrets["refresh_token"])
+		else:
+			oauth_flow = dropbox.oauth.DropboxOAuth2FlowNoRedirect(
+				self.secrets["app_key"], self.secrets["app_secret"], token_access_type="offline")
+			authorize_url = oauth_flow.start()
+			print(f"1. Go to {authorize_url}")
+			print("2. Click 'Allow (you might have to log in first).")
+			print("3. Copy the authorization code.")
+			auth_code = input("Enter the authorization code here: ")
+			oauth_result = oauth_flow.finish(auth_code)
+			access_token = oauth_result.access_token
+			self.dbx = dropbox.Dropbox(access_token)
+			self.secrets["refresh_token"] = oauth_result.refresh_token
+			self._save_secrets()
 
 	def _upload_file(self, content: bytes):
 		mode = WriteMode("overwrite" if self.overwrite else "add")
@@ -40,7 +70,7 @@ class Backdropper:
 		finally:
 			os.remove(zipname)
 
-	def upload(self, target, path, overwrite=False):
+	def upload(self, target: str, path: str, overwrite: bool = False):
 		"""
 		:param str target: Filename or directory name to upload. A directory will be zipped before being uploaded.
 		:param str path: The path/filename to save the target under.
@@ -58,25 +88,19 @@ class Backdropper:
 			print("Invalid input")
 
 
-def get_access_token(token_filename: str) -> str:
-	with open(token_filename) as f:
-		at: str = f.read().strip()
-	return at
-
-
 def create_parser() -> argparse.Namespace:
 	parser = argparse.ArgumentParser(description=__doc__)
 	parser.add_argument("target", action="store", help="The file or directory to upload")
 	parser.add_argument("-n", "--name", action="store", help="Alternative path/name to save the target under")
 	parser.add_argument("--do-not-overwrite", action="store_false", help="Do not overwrite name if this flag is set")
-	parser.add_argument("-t", "--token-file", action="store", default=ACCESS_TOKEN_FILENAME,
-	                    help=f"Read token from file. By default look for {ACCESS_TOKEN_FILENAME} in current directory")
+	parser.add_argument("-s", "--secrets-filename", action="store", default=SECRETS_FILENAME,
+						help=f"Read secrets from file. By default look for {SECRETS_FILENAME} in current directory")
 	return parser.parse_args()
 
 
 if __name__ == "__main__":
 	args = create_parser()
-	bd = Backdropper(get_access_token(args.token_file))
+	bd = Backdropper(args.secrets_filename)
 	save_name = args.name or args.target
 	bd.upload(args.target, save_name, args.do_not_overwrite)
 	print(f"Saving {args.target} to {save_name} {'completed successfully' if bd.success else 'failed'}")
